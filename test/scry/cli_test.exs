230db7df1d1c0c7111495d16975a4c083812b275
br4inii <moshericoe@gmail.com>
2019-01-07 00:11:43 +0300

    test assigned command examples

---
+++ test/scry/cli_test.exs
@@ -0,0 +1,30 @@
+defmodule Scry.CLITest do
+  @moduledoc """
+  This module holds the tests for the Command Line Interface
+  for the various commands parsed to Scry app.
+  """
+  use ExUnit.Case
+  import ExUnit.CaptureIO
+
+  alias Scry.CLI
+  @test_data ["--help"]
+  @rate ["--btc","rate"]
+  @time ["--btc","time"]
+
+  test "command '--help' outputs the usge info" do
+    assert CLI.parse_params(@test_data) == :help
+    assert CLI.process(:help)== :ok
+    #assert capture_io(CLI.process(:help)) == "SCRY\n------\n#todo\n\n"
+  end
+
+  test "command '--btc time' outputs time of recorded btc price" do
+    assert CLI.parse_params(@time) == :btctime
+    assert CLI.process(:btctime)== :ok
+  end
+
+  test "command '--btc rate' outputs rate of btc in USD" do
+    assert CLI.parse_params(@rate) == :btcrate
+    assert CLI.process(:btcrate) == :ok
+  end
+
+end
